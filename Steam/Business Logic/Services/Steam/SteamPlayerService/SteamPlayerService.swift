3fe39caf8dafd06e04ff81b1c0a07a883ad1c9cd
Ivan Zinovyev <zinovyev-ivan@yandex.ru>
2019-11-23 18:05:12 +0300

    Remove redundant requests

--- Steam/Business Logic/Services/Steam/SteamPlayerService/SteamPlayerService.swift
+++ Steam/Business Logic/Services/Steam/SteamPlayerService/SteamPlayerService.swift
@@ -24,28 +24,10 @@ class SteamPlayerService {
         }
     }
     
-    func getSteamLevel(steamId: String,
-                       completion: @escaping (Int) -> Void) {
-        
-        ApiService.shared.getSteamLevel(steamId: steamId) {
-            completion($0.response.playerLevel)
-        }
-    }
-    
     func getBadges(steamId: String,
                    completion: @escaping (BadgesResponse) -> Void) {
         
         ApiService.shared.getBadges(steamId: steamId) {
-            completion($0.response)
-        }
-    }
-    
-    func getCommunityBadgeProgress(steamId: String,
-                                   badgeId: Int,
-                                   completion: @escaping (CommunityBadgeProgressResponse) -> Void) {
-        
-        ApiService.shared.getCommunityBadgeProgress(steamId: steamId,
-                                                    badgeId: badgeId) {
             completion($0.response)
         }
     }
